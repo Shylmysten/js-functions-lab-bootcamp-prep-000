f3aa19eb587334fe970e327340f16a0deee8e838
Tony Boswell <tboswell@kc.rr.com>
2017-11-09 05:22:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
 // write your code below!
-function happyHolidays() {
-  console.log("Happy Holidays!");
-}
-function happyHolidaysTo(name)  {
-  return `Hppy Holidays, ${name}!`
-}
-function happyHolidayTo(holiday, name) {
-  return  `Happy ${holiday}, ${name}!`
-}
-function holidayCountdown(holiday, days) {
-  return  `It's ${days} days until ${holiday}!`
-}
